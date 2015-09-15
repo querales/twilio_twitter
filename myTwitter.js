ed88f4578ce04326ceb478b094769633c3ba9b34
Gabriella <gabriella.querales@datasift.com>
2015-09-15 15:59:42 -0700

    Procfile

--- myTwitter.js
+++ myTwitter.js
@@ -1,6 +1,12 @@
 var lastTweet;
 
 var Twitter = require('twitter');
+
+// If undefined in our process load our local file
+// (i.e. we aren't on an external server where we set these differently)
+if(!process.env.TWITTER_CONSUMER_KEY) {
+  var env = require('./env.js')
+}
  
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,6 +17,8 @@ var client = new Twitter({
 
 // this function gets the last tweet for the handle passed in the parm
 exports.getTweet = function getTweet (handle,setMessg){
+//this line bellow is to test. commenedted the line above need to uncoment the above line to runnormal
+//function getTweet (handle,setMessg){	
 client.get('users/lookup.json', {screen_name:handle}, function(error,tweet,response){
 	   if (error)
 	   {
@@ -20,9 +28,11 @@ client.get('users/lookup.json', {screen_name:handle}, function(error,tweet,respo
 	   else
 	   {	
 		  // console.log("type "+typeof (tweet));  
-		  // console.log(tweet[0].status.text);
+		  console.log(tweet[0].status.text);
 		  lastTweet =tweet[0].status.text;
-		  setMessg(lastTweet);
+		 // setMessg(lastTweet);
 	   }
 	})
 };
+
+//getTweet ("levie")
--- twilio.js
+++ twilio.js
@@ -1,3 +1,8 @@
+if(!process.env.TWILIO_ACCOUNT_SID) {
+  var env = require('./env.js')
+}
+
+
 // Load configuration information from system environment variables.
 var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
     TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
