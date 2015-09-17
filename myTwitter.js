b462b5ab449ff62f624ed0b21c97806150c2c3e4
Gabriella <gabriella.querales@datasift.com>
2015-09-17 15:49:36 -0700

    update twilio

--- myTwitter.js
+++ myTwitter.js
@@ -1,3 +1,15 @@
+// appdynamics agent
+/*
+require("appdynamics").profile({
+    controllerHostName: 'paid138.saas.appdynamics.com',
+    controllerPort: 80, // If SSL, be sure to enable the next line     controllerSslEnabled: true // Optional - use if connecting to controller via SSL  
+    accountName: 'BringIT',
+    accountAccessKey: '2tljw7jnvndg',
+    applicationName: 'twitter_twilio',
+    tierName: 'twitter',
+    nodeName: 'twitter' // The controller will automatically append the node name with a unique number
+});
+*/
 var lastTweet;
 
 var Twitter = require('twitter');
